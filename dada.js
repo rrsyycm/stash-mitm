7ea5b559f7965812f9ae56d33262b09727838638
rrsyycm <rrsyycm@163.com>
2025-09-29 17:03:48 +0800

    fix(dada):修复脚本日志和通知内容
    - 移除多余的$response日志输出
    - 修改通知内容为固定字符串'body'
    - 更新脚本版本号从0.0.1到0.0.2
    - 更新脚本URL的时间戳参数

--- dada.js
+++ dada.js
@@ -1,5 +1,4 @@
 console.log("Dada-脚本启动");
-console.log($response);
 console.log($response.body);
-$notification.post('达达拦截', 'subtitle', $response.body)
+$notification.post('达达拦截', 'subtitle', 'body')
 $done({});
